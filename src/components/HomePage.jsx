6646d1cdb200ba95872998a475cae9e7df1662a1
gitguy33 <gebgoe@gmail.com>
2025-01-07 13:31:37 +0530

    commit

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,52 @@
-import { Button } from "react-bootstrap";
+import { useState } from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from './OIP.jpeg'; // Make sure to import your image correctly
 
-export const Home=()=>{
+export const Home = () => {
 
+  const [index, setIndex] = useState(0);
 
-    return(
-        <div>
-            
-        </div>
-    );
-}
+  const handleSelect = (selectedIndex) => {
+    setIndex(selectedIndex);
+  };
+
+  return (
+    <div>
+      <Carousel activeIndex={index} onSelect={handleSelect}>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={ExampleCarouselImage}
+            alt="First slide"
+          />
+          <Carousel.Caption>
+            <h3>First slide label</h3>
+            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={ExampleCarouselImage}
+            alt="Second slide"
+          />
+          <Carousel.Caption>
+            <h3>Second slide label</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+        <Carousel.Item>
+          <img
+            className="d-block w-100"
+            src={ExampleCarouselImage}
+            alt="Third slide"
+          />
+          <Carousel.Caption>
+            <h3>Third slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      </Carousel>
+    </div>
+  );
+};

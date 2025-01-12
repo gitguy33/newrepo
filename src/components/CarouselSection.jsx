ea570a136bd1e0831b0d67564acff8ef9e911128
gitguy33 <gebgoe@gmail.com>
2025-01-12 15:42:32 +0530

    commit

--- src/components/CarouselSection.jsx
+++ src/components/CarouselSection.jsx
@@ -57,6 +57,18 @@ function CarouselSection(){
             <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
         </Carousel.Caption>
         </Carousel.Item>
+
+        <Carousel.Item>
+        <img
+            className="d-block w-100"
+            src={ExampleCarouselImage}
+            alt="Third slide"
+        />
+        <Carousel.Caption>
+            <h3>Fifth slide label</h3>
+            <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+        </Carousel.Caption>
+        </Carousel.Item>
         </Carousel>
     );
 }

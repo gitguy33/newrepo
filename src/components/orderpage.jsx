797596a3798cea0d1e7bc5bb8f671528e0028a31
gokulgs70 <gokulgopakumar490@gmail.com>
2025-01-12 14:36:01 +0530

    added css to order page

--- src/components/orderpage.jsx
+++ src/components/orderpage.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import './OrderPage.css';
+import truck from './images/freepik__background__47830.png'
 
 const Order = () => {
     const [showModal, setShowModal] = useState(false);
@@ -13,47 +14,91 @@ const Order = () => {
     };
 
     return (
-        <div>
+    <div>
+        <div className='outer_border'>
             <form>
                 <div className="place-order-left">
-                    <p className="title">Delivery information</p>
-                    <div className="multi-fields">
-                        <input type="text" placeholder='First Name'/>
-                        <input type="text" placeholder='Last Name'/>
+                    <h2 className="title">Enter delivery information</h2>
+                    <div className='order_outer'>
+                    <div className="order_fields1" style={{float:"left",margin_right:"20px"}}>
+                        <label className='orderinput_label'> First Name</label>
+                        <input className='order_input' type="text" placeholder='First Name'/>
                     </div>
-                    <input type="email" placeholder='Email Address' />
-                    <input type="text" placeholder='Street' />
-                    <div className="multi-fields">
-                        <input type="text" placeholder='City'/>
-                        <input type="text" placeholder='State'/>
+                    <div style={{float:"inherit"}}>
+                        <label className='orderinput_label'>Last Name</label>
+                        <input className='order_input' type="text" placeholder='Last Name'/>
                     </div>
-                    <div className="multi-fields">
-                        <input type="text" placeholder='Zip code'/>
-                        <input type="text" placeholder='Country'/>
+                    <div className="order_fields2" style={{float:"left",margin_right:"20px"}}>
+                        <label className='orderinput_label'> Enter Email</label>
+                        <input className='order_input' type="email" placeholder='Email Address'/>
                     </div>
-                    <input type="text" placeholder='Phone' />
+                    <div style={{float:"inherit"}}>
+                        <label className='orderinput_label'>Enter Street</label>
+                        <input className='order_input' type="text" placeholder='Street'/>
+                    </div>
+                    <div className="order_fields3" style={{float:"left",margin_right:"20px"}}>
+                        <label className='orderinput_label'> Enter City</label>
+                        <input className='order_input' type="text" placeholder='City' />
+                    </div>
+                    <div style={{float:"t"}}>
+                        <label className='orderinput_label'>Enter State</label>
+                        <input className='order_input' type="text" placeholder='state'/>
+                    </div>
+                    <div className="order_fields4" style={{float:"left",margin_right:"20px"}}>
+                        <label className='orderinput_label'> Enter Zip code</label>
+                        <input className='order_input' type="text" placeholder='Zip code'/>
+                    </div>
+                    <div style={{float:"inherit"}}>
+                        <label className='orderinput_label'>Enter Country</label>
+                        <input className='order_input' type="text" placeholder='Country'/>
+                    </div>
+                    <div className="order_fields5" style={{float:"left",margin_right:"20px"}}>
+                        <label className='orderinput_label'> Enter phone number</label>
+                        <input className='order_input' type="text" placeholder='Phone number'/>
+                    </div>
+                    </div>
                 </div>
-                <div className="place-order-right">
+                <div className="order_price">
                     <div className="cart-total">
-                        <h2>Cart Total</h2>
+                        <h2>Order</h2>
                         <div>
                             <div className="cart-total-details">
-                                <p>Subtotal</p>
-                                <p>₹ 100</p>
+                                <p>Items                     ₹ 100</p>
                             </div>
-                            <hr/>
                             <div className="cart-total-details">
-                                <p>Delivery Charge</p>
-                                <p>₹ 20</p>
+                                <p>Delivery Charge : ₹ 20</p>
                             </div>
-                            <hr/>
                             <div className="cart-total-details">
-                                <b>Total</b>
-                                <p>₹ 120</p>
+                                <p>Sub Tota: ₹ 120</p>
                             </div>
                         </div>
-                        <button type="button" className="proceed-button" onClick={handleProceedToPay}>Proceed to Pay</button>
                     </div>
+                </div>
+                <div className='payment_main'>
+                <h3>Choose Payment Method</h3>
+
+                <div className="radio-input" style={{float:"inherit"}}>
+                <input value="value-1" name="value-radio" id="value-1" type="radio" />
+                <label htmlFor="value-1">
+                <div className="text">
+                <span className="circle"></span>
+                 Card
+                </div>
+                <div className="price">
+                </div>
+                </label>
+                <input value="value-2" name="value-radio" id="value-2" type="radio" />
+                <label htmlFor="value-2">
+                <div className="text">
+                <span className="circle"></span>
+                UPI
+                </div>
+                <div className="price">
+                </div>
+            </label>
+            </div>
+
+                <button type="button" className="proceed-button" onClick={handleProceedToPay}>Proceed to Pay</button>
                 </div>
             </form>
 
@@ -69,6 +114,8 @@ const Order = () => {
                     </div>
                 </div>
             )}
+            <img src={truck}  className='truck_image' alt='#'/>
+            </div>
         </div>
     );
 };

f1d2e36426a0c6d400cace5a4932b3adc7078053
rithick03r <rithick0003@gmail.com>
2025-01-14 16:31:43 +0530

    changes to main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Order from './components/orderpage'; // Import Order page
+import { Home } from './components/homepage'; // Import Home page
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} /> {/* Home page route */}
+          <Route path="/order" element={<Order />} /> {/* Order page route */}
+        </Routes>
+      </Router>
     </div>
   );
 }

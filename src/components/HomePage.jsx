ed5b3d08c87fa3e3102707a626ad7aada4a9e96b
rithick03r <rithick0003@gmail.com>
2025-01-14 19:33:59 +0530

    updated the my order in home

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import Carousel from 'react-bootstrap/Carousel';
-import ExampleCarouselImage from './BG.jpg'; // Make sure to import your image correctly
+import ExampleCarouselImage from './images/BG.jpg';
+// import ExampleCarouselImage from './BG.jpg'; // Make sure to import your image correctly
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,9 +9,10 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { IoMdLogIn } from "react-icons/io";
-import { Link } from 'react-router-dom'; // Import Link from react-router-dom
+import { Link } from 'react-router-dom';
 
 export const Home = () => {
+
   const [index, setIndex] = useState(0);
 
   const handleSelect = (selectedIndex) => {
@@ -20,51 +22,47 @@ export const Home = () => {
   return (
     <div>
       <div>
-        <Navbar bg="light" data-bs-theme="light">
-          <Container>
-            <Navbar.Brand href="#home">QuickCart</Navbar.Brand>
-            <Nav className="me-auto">
-              <Nav.Link href="#home">Home</Nav.Link>
+      <Navbar bg="light" data-bs-theme="light">
+        <Container>
+          <Navbar.Brand href="#home">QuickCart</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            {/* <Nav.Link href="#features">MyOrders</Nav.Link> */}
+            <div style={{ marginLeft: "50px" }}>
+              <Nav.Link as={Link} to="/order">My Orders</Nav.Link>
+            </div>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#pricing">AboutUs</Nav.Link>
 
-              {/* MyOrders link with unchanged style */}
-              <div style={{ marginLeft: "50px" }}>
-                <Nav.Link as={Link} to="/order">
-                  My Orders
-                </Nav.Link>
-              </div>
+            <div name="search" style={{marginLeft:"200px"}}>
+            <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success">Search</Button>
 
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-              <Nav.Link href="#pricing">AboutUs</Nav.Link>
+            </Form>
+            </div>
 
-              <div name="search" style={{ marginLeft: "200px" }}>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-              </div>
+            <div style={{marginLeft:"200px"}}>
+              
+              <Nav.Link href="#pricing"> <IoMdLogIn /> LOGIN</Nav.Link>
+            
+            </div>
 
-              <div style={{ marginLeft: "200px" }}>
-                <Nav.Link href="#pricing">
-                  <IoMdLogIn /> LOGIN
-                </Nav.Link>
-              </div>
-
-              <div style={{ marginLeft: "50px" }}>
-                <Nav.Link href="#pricing">
-                  <MdOutlineShoppingCart /> CART
-                </Nav.Link>
-              </div>
-            </Nav>
-          </Container>
-        </Navbar>
+            <div style={{marginLeft:"50px"}}>
+              <Nav.Link href="#pricing"><MdOutlineShoppingCart /> CART</Nav.Link>  
+            </div>
+          </Nav>
+        </Container>
+      </Navbar>
       </div>
 
       <Carousel activeIndex={index} onSelect={handleSelect}>
+        
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -88,7 +86,7 @@ export const Home = () => {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
           </Carousel.Caption>
         </Carousel.Item>
-
+        
         <Carousel.Item>
           <img
             className="d-block w-100"
@@ -105,7 +103,7 @@ export const Home = () => {
           <img
             className="d-block w-100"
             src={ExampleCarouselImage}
-            alt="Fourth slide"
+            alt="Third slide"
           />
           <Carousel.Caption>
             <h3>Fourth slide label</h3>
@@ -116,3 +114,5 @@ export const Home = () => {
     </div>
   );
 };
+
+
